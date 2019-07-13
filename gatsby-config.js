c52cb5109eec79e83dcebf8300b981e0806f2e6f
Alex Bavinton <alex.bavinton@yld.io>
2019-07-13 14:08:12 +0100

    fixes to make gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,42 +9,19 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-mdx',
-      options: {
-        defaultLayouts: {
-          default: require.resolve('./src/components/layout.js'),
-        },
-        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: 'posts',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: 'images',
-      },
-    },
+    // {
+    //   resolve: 'gatsby-mdx',
+    //   options: {
+    //     defaultLayouts: {
+    //       default: require.resolve('./src/components/layout.js'),
+    //     },
+    //     gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+    //   },
+    // },
     {
       resolve: 'gatsby-source-instagram',
       options: {
-        username: 'gatsbyjs',
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
-      options: {
-        production: true,
-        disable: !process.env.ANALYZE_BUNDLE_SIZE,
-        generateStatsFile: true,
-        analyzerMode: 'static',
+        username: 'alexbavinton',
       },
     },
   ],
