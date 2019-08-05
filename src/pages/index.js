719e423392434b643fdadcee221544bbcae941ba
Alex Bavinton <alexwb@hotmail.co.uk>
2019-08-05 18:13:09 +0100

    WIP builds hero banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 
-const index = () => <Layout></Layout>;
+const index = () => (
+  <Layout>
+    <Hero />
+  </Layout>
+);
 
 export default index;
