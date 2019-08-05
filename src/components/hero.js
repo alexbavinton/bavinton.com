a277a2ced0b50da165ffe13c9a9b17a58c277b4e
Alex Bavinton <alexwb@hotmail.co.uk>
2019-08-05 18:13:20 +0100

    WIP builds hero banner

---
+++ src/components/hero.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { css } from '@emotion/core';
+import {
+  backgroundLight,
+  darkAccent,
+  backgroundDark,
+  lightGrey,
+} from '../components/colors';
+
+const heroStyles = css`
+  height: 90vh;
+  background-image: url('images/backgroundHero.png');
+  background-size: stretch;
+  background-position: top 20% center;
+  .copy {
+    background-image: linear-gradient(
+      to top,
+      ${backgroundLight}dd 2rem,
+      ${backgroundLight}00
+    );
+    text-shadow: 2px 2px 1px ${backgroundDark};
+    h3 {
+      color: ${lightGrey};
+    }
+  }
+`;
+
+const Hero = () => (
+  <div css={heroStyles}>
+    <div className="copy">
+      <h1>Alex Bavinton</h1>
+      <h3>Software Engineer</h3>
+    </div>
+  </div>
+);
+
+export default Hero;
